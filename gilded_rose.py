eed15659a7856d19864d9332b1db2843ded86884
Elliot Iddon <elliot@land.tech>
2022-09-09 11:32:52 +0100

    Initial progress converting to a chain of applications

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,3 +1,4 @@
+from typing import Callable
 # -*- coding: utf-8 -*-
 
 SULFURAS_NAME = "Sulfuras, Hand of Ragnaros"
@@ -9,35 +10,47 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    @staticmethod
+    def increment_quality_if(item: "Item", condition: Callable[["Item"], bool]):
+        if condition(item):
+            GildedRose.increment_quality(item)
+
+
+    @staticmethod
+    def increment_quality(item: "Item"):
+        if item.quality < 50:
+            item.quality += 1
+
+    @staticmethod
+    def decrement_quality(item: "Item"):
+        if item.quality > 0:
+            item.quality -= 1
+
     def update_quality(self):
         for item in self.items:
             if item.name == SULFURAS_NAME:
-                pass # Sulfuras is legendary, it never changes value or has to be sold
+                continue # Sulfuras is legendary, it never changes value or has to be sold
             elif item.name == AGED_BRIE_NAME:
                 item.sell_in -= 1
-                if item.quality < 50:
-                    item.quality += 1
-                if item.sell_in < 0 and item.quality < 50:
-                    item.quality += 1
+                self.increment_quality(item)
+                if item.sell_in < 0:
+                    self.increment_quality(item)
             elif item.name == BACKSTAGE_PASS_NAME:
-                if item.quality < 50:
-                    item.quality += 1
-                    if item.sell_in <= 10:
-                        if item.quality < 50:
-                            item.quality += 1
-                    if item.sell_in <= 5:
-                        if item.quality < 50:
-                            item.quality += 1
+                # apply(increment, increment_if, increment_if, decrement_sell_by, zero_if_passed)
+                self.increment_quality(item)
+                self.increment_quality_if(item, lambda i: i.sell_in <= 10)
+                self.increment_quality_if(item, lambda i: i.sell_in <= 5)
+
                 item.sell_in -= 1
                 if item.sell_in < 0:
                     item.quality = 0
             else:
-                if item.quality > 0:
-                    item.quality -= 1
+                self.decrement_quality(item)
                 item.sell_in -= 1
                 if item.sell_in < 0:
-                    if item.quality > 0:
-                        item.quality -= 1
+                    self.decrement_quality(item)
+
+
 
 
 class Item:
